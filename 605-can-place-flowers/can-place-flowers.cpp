828a0dfa15ea42b08355d857278d06d629f1e2c7
shivam kumar <shivam.kr2k7@gmail.com>
2026-08-27 14:24:35 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (6.45%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -6,7 +6,7 @@ public:
             if(flowerbed[i] == 0){
                 if(i == 0 || flowerbed[i-1] == 0){
                     if(i == flowerbed.size()-1 || flowerbed[i+1] == 0){
-                        flowerbed[i] += 1;
+                        flowerbed[i] = 1;
                         n--;
                         if(n<=0){
                             return true;
