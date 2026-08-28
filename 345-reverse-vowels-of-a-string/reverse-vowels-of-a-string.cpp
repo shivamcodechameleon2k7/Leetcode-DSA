5fce34366f1009a0b3a025f9c0ec74f013ce296c
shivam kumar <shivam.kr2k7@gmail.com>
2026-08-28 13:31:18 +0530

    Time: 7 ms (7.69%) | Memory: 11.4 MB (5.26%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    // bool isVowel(char c){
-    //     return string("aeiouAEIOU").find(c) != string::npos;
-    // }
-    bool isVowel(char c) {
-    return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
-           c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
+    bool isVowel(char c){
+        return string("aeiouAEIOU").find(c) != string::npos;
     }
+    // bool isVowel(char c) {
+    // return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
+    //        c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
+    // }
     string reverseVowels(string s) {
         int left = 0;
         int right = s.size()-1;
