fa25c70d651bbd482d7f7234c17981123589692c
Shivam Kumar <shivam.kr2k7@gmail.com>
2026-09-08 15:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.09%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // transpose of the matrix 
         for(int i = 0; i < matrix.size();i++){
             for( int j = i+1;j<matrix.size();j++){
                     swap(matrix[i][j],matrix[j][i]);
                 } 
+            // reverse the elements in the matrix after transpose to there corresponding position.
             int left = 0;
             int right = matrix.size()-1;
             while(left<right){
