e03037e3877af75eb6b03087cac3e5c0a319055f
Shivam Kumar <shivam.kr2k7@gmail.com>
2026-09-09 15:45:52 +0530

    Time: 2734 ms (6.39%) | Memory: 34.3 MB (88.07%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0;
-        for(int i  = 0 ; i < nums.size();i++){
+        for(int i = 0 ; i < nums.size();i++){
             int sum = 0;
+            // i chooses the starting point.
+            // j chooses the ending point.
+            // sum   = "Does this subarray equal k?"
+            // count = "How many such subarrays did I find?"
             for(int j = i; j < nums.size();j++){
                 sum += nums[j];
                 if(sum == k){
