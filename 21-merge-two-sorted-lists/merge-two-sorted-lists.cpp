d11410de8952a53f4d9578f685af31486bdcc332
Shivam Kumar <shivam.kr2k7@gmail.com>
2026-09-12 14:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (62.56%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -8,17 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//  if you replace head1 and head2 to list1 and list2 
+// then replace all the head1 and head2 to list1 and list2 in the program
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
         if(head1 == NULL || head2 == NULL){
             return head1 == NULL ? head2 : head1;
+            // IF HEAD1 == NULL 
+            // RETURN HEAD2 ELSE RETURN HEAD1
         }
-
+        // CASE 1
         if(head1->val <= head2->val){
             head1->next = mergeTwoLists(head1->next,head2);
             return head1;
         }
+        // CASE2
         else{
             head2->next = mergeTwoLists(head1,head2->next);
             return head2;
