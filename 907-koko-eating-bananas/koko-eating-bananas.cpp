48076c08d9dbfa0233128b471771b36e442c0e66
Shivam Kumar <shivam.kr2k7@gmail.com>
2026-09-19 17:05:11 +0530

    Time: 7 ms (69.42%) | Memory: 23 MB (24.41%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -3,10 +3,12 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
+        // maximum of vector data is found using above code snippet
         int n = piles.size();
         while (low <= high){
             int mid = low + (high - low)/2;
             long long totalhours = 0;
+            // to prevent overflow long long is used
             for(int i = 0; i < n; i++){
                 // ceiling-division formula
                 int hours = (piles[i] + mid - 1)/mid;
